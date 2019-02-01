50ab7c377f8d6e66131b067e0803c89ca0af81a2
LokaPrakash <lokaprakashraju.p@gmail.com>
2019-02-01 18:21:17 +0530

    Update create-employee.component.ts

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Department } from '../models/department.model';
+import { Employee } from '../models/employee.model'; 
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
+  employee: Employee = {
+    id: null,
+    name: null,
+    gender: null,
+    contactPreference: null,
+    phoneNumber: null,
+    email: null,
+    dateOfBirth: null,
+    department: '-1',
+    isActive: null,
+    photoPath: null,
+  }; 
 previewPhoto :boolean = false; 
   constructor() { }
 departments: Department[] = [
